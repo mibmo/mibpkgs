c6e76e4719dd9e545d787da761f3152f7980d5ba
mib <mib@kanp.ai>
2023-04-09 20:02:40 +0200

    initial commit
    includes first package: maelstrom! :D

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "mib's smol nixpkgs repo";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    clj-nix = {
+      url = "github:jlesquembre/clj-nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, clj-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cljpkgs = clj-nix.packages."${system}";
+      in
+      {
+        packages = {
+          maelstrom = pkgs.callPackage ./maelstrom { inherit cljpkgs; };
+        };
+      });
+}
---
+++ maelstrom/default.nix
@@ -0,0 +1,16 @@
+{ fetchFromGitHub, cljpkgs, openjdk19, jdk17_headless }:
+cljpkgs.mkCljBin rec {
+  name = "maelstrom";
+  version = "0.2.3";
+  projectSrc = fetchFromGitHub {
+    owner = "jepsen-io";
+    repo = name;
+    rev = "v${version}";
+    sha256 = "sha256-nZS2oVF/m3yGU+HFD1sXdZbZaOfxYm3HMrxuCRZ0VMg=";
+  };
+  buildCommand = "lein uberjar";
+  main-ns = "maelstrom.core";
+  lockfile = ./deps-lock.json;
+  jdkRunner = openjdk19;
+  jarPath="target/${name}-${version}-standalone.jar";
+}
